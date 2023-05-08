bc0e1807b0c2f2dba7f93c6a740fa6c50440f69c
Jatin Katap <jatinvbaisolutions@gmail.com>
2023-05-08 15:42:45 +0530

    make public

---
+++ App.js
@@ -0,0 +1,34 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App=()=>{
+  return(
+    <View>
+      <Text style={{fontSize:30}}>Hello</Text>
+    </View>
+  )
+}
+export default App;
